82c63343179b06932851cb5a7df7f4601278efb3
David Brooks <davidbrooks.now@gmail.com>
2014-01-25 15:54:49 -0500

    initial commit

---
+++ chorerocket/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chorerocket::Application.config.secret_key_base = 'a28c5a365685f832f37b6a6f56c1364f37cbe94a575b509484e490eeb7017c311acbb0d039eef99b759ea33910cda1b4b2ca846f7151038a05c25c1b94b80513'
---
+++ chorerocket/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chorerocket::Application.config.session_store :cookie_store, key: '_chorerocket_session'
